160f8ba580101ad322e07817368d5b888247e2ee
Rahmat Hidayat <rh.hie.dayatt@gmail.com>
2018-03-02 20:20:28 +0700

    06.js modified

--- 06.js
+++ 06.js
@@ -1,24 +1,19 @@
-let words = ['Aku', 'ingin', 'begini', 'Aku', 'ingin', 'begitu', 'Ingin', 'ini', 'itu', 'banyak', 'sekali', 'Semua', 'semua', 'semua', 'Dapat', 'dikabulkan', 'Dapat', 'dikabulkan', 'Dengan', 'kantong', 'ajaib', 'Aku', 'ingin', 'terbang', 'bebas', 'Di', 'angkasa', 'Hei…', 'baling', 'baling', 'bambu', 'La...', 'la...', 'la...', 'Aku', 'sayang', 'sekali', 'Doraemon', 'La...', 'la...', 'la...', 'Aku', 'sayang', 'sekali'];
-console.log("Jumlah kata ada "+words.length);
-let jumlah = 0;
+const lyrics = `Aku ingin begini
+Aku ingin begitu
+Ingin ini itu banyak sekali
+Semua semua semua
+Dapat dikabulkan
+Dapat dikabulkan
+Dengan kantong ajaib
+Aku ingin terbang bebas
+Di angkasa
+Hei… baling baling bambu
+La... la... la...
+Aku sayang sekali
+Doraemon
+La... la... la...
+Aku sayang sekali`
 
-for (let i=0; i<=words.length; i++) {
-  if (words[i] == 'Aku') {
-    jumlah = jumlah + 1;
-  }
-}
-console.log(`Jumlah kata "Aku" adalah `+jumlah);
-
-for (let i=0; i<=words.length; i++) {
-  if (words[i] == 'ingin') {
-    jumlah = jumlah + 1;
-  }
-}
-console.log(`Jumlah kata "ingin" adalah `+jumlah);
-
-for (let i=0; i<=words.length; i++) {
-  if (words[i] == 'dapat') {
-    jumlah = jumlah + 1;
-  }
-}
-console.log(`Jumlah kata "dapat" adalah `+jumlah);
+console.log('aku: ' + lyrics.match(/aku/gi).length)
+console.log('ingin: ' + lyrics.match(/ingin/gi).length)
+console.log('dapat: ' + lyrics.match(/dapat/gi).length)
